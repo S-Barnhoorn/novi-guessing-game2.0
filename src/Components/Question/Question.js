0a10721dca73a825e5af25dc77b42c6948ccefe3
Albatroaz7 <sambarnhoorn@gmail.com>
2021-09-30 14:04:05 +0200

    added useEffect so when you go the next question, the radiobox isn't already checked.

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,39 +1,50 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
-const Question = ({ data, onAnswerUpdate, numberOfQuestions, onSetCurrentQuestion, currentQuestion, onSetStep}) => {
+const Question = ({data, onAnswerUpdate, numberOfQuestions, onSetCurrentQuestion, currentQuestion, onSetStep}) => {
     const [selected, setSelected] = useState('');
     const [error, setError] = useState('');
 
+    const radioCheck = useRef();
+
+    useEffect(() => {
+        const checkedInput = radioCheck.current.querySelector('input:checked')
+        if (checkedInput) {
+            checkedInput.checked = false
+        }
+    }, [data]);
 
     const changeHandler = (e) => {
         setSelected(e.target.value);
     }
 
     const nextQuestion = (e) => {
-        if(selected === ''){
+        if (selected === '') {
             setError('Please select one option!');
         }
-        onAnswerUpdate(prevState => [...prevState, { question: data.questionText, answer: selected}])
+        onAnswerUpdate(prevState => [...prevState, {question: data.questionText, answer: selected}])
         setSelected('');
-        if(currentQuestion < numberOfQuestions -1){
+        if (currentQuestion < numberOfQuestions - 1) {
             onSetCurrentQuestion(currentQuestion + 1);
-        } else{
+        } else {
             onSetStep(3);
         }
     }
 
 
 
+
     return (
         <div>
             <h2>{data.questionText}</h2>
-            {data.answerOptions.map((choice, i) =>
-                <label key={i}>
-                    <input type="radio" name="answer" value={choice} onChange={changeHandler}/>
-                    {choice}
-                </label>
-            )}
-            <div>error here</div>
+            <div ref={radioCheck}>
+                {data.answerOptions.map((choice, i) =>
+                    <label key={i}>
+                        <input type="radio" name="answer" value={choice} onChange={changeHandler}/>
+                        {choice}
+                    </label>
+                )}
+            </div>
+            {error && <div>{error}</div>}
             <button type="button" onClick={nextQuestion}>Next</button>
 
         </div>
